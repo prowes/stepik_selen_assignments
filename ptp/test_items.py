8e585e768492c4f618259dcc04af3a7ce36eac82
prowes <34424042+prowes@users.noreply.github.com>
2020-04-29 18:59:00 +0300

    Add files via upload
    Test name fixed

--- ptp/test_items.py
+++ ptp/test_items.py
@@ -1,3 +1,3 @@
 
-def test_guest_should_see_login_link_pass(browser):
+def test_add_to_basket_button_visible(browser):
     assert browser.find_element_by_class_name("btn-add-to-basket"), "Add to basket button not found"
